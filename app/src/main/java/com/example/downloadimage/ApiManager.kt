b883cd89a7b5c7e742149ffef194d25ab5b483b6
Hoan Le <hoanle@xtaypro.com>
2020-06-29 00:59:57 +0700

    Return to original data set

--- app/src/main/java/com/example/downloadimage/ApiManager.kt
+++ app/src/main/java/com/example/downloadimage/ApiManager.kt
@@ -11,9 +11,10 @@ class ApiManager {
         return Single.create {
             val jsonString = """
             {'title':'Civil War',
-            'image':['http://movie.phinf.naver.net/20151127_84/1448585272016tiBsF_JPEG/movie_image.jpg?type=m665_443_2',
-            'http://movie.phinf.naver.net/20151125_36/1448434523214fPmj0_JPEG/movie_image.jpg?type=m665_443_2', 
-            'https://upload.wikimedia.org/wikipedia/commons/7/7a/Salzburg_from_Gaisberg_big_version.jpg','']}
+            'image':[
+            'http://movie.phinf.naver.net/20151127_272/1448585271749MCMVs_JPEG/movie_image.jpg?type=m665_443_2',
+            'http://movie.phinf.naver.net/20151127_84/1448585272016tiBsF_JPEG/movie_image.jpg?type=m665_443_2',
+            'http://movie.phinf.naver.net/20151125_36/1448434523214fPmj0_JPEG/movie_image.jpg?type=m665_443_2']}
             """.trimIndent()
             try {
                 val jsonObject = JSONObject(jsonString)
